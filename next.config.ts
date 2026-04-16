006077a35e1e91001251a601ac49ab95d44c32a5
juicenewera <sf.prod.sf3@gmail.com>
2026-04-16 20:22:07 -0300

    fix(build): purge all deprecated devIndicators properties from next.config.ts satisfying Next.js 16 requirements

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     qualities: [70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 75, 85],
   },
-  devIndicators: {
-    buildActivity: false,
-  },
 };
 
 export default nextConfig;
